7e0a8301a3b3bf75e3c7b589c9e815b1e5f7ca02
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-07-17 17:24:22 +0200

    Ajoutez des fichiers projet.

---
+++ TemplatingRazor/Controllers/ExpenditureController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TemplatingRazor.Models
+
+namespace TemplatingRazor.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Expenditure> expenditures = new List<Expenditure>()
+            {
+                new Expenditure(1, "Adieu ma retraite", new DateTime(2024, 7, 17), 25000.50)
+            };
+            return View();
+        }
+    }
+}
---
+++ TemplatingRazor/Models/Expenditure.cs
@@ -0,0 +1,18 @@
+﻿namespace TemplatingRazor.Models
+{
+    public class Expenditure
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+        public Expenditure(int id, string name, DateTime date, double price)
+        {
+            Id = id;
+            Name = name;
+            Date = date;
+            Price = price;
+        }
+    }
+
+}
