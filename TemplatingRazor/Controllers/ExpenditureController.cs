080ccd416ee4ca7abbedaf16113ede39714b2a5d
Aeldro <nodepuydt@gmail.com>
2024-07-18 12:09:36 +0200

    Bla

--- TemplatingRazor/Controllers/ExpenditureController.cs
+++ TemplatingRazor/Controllers/ExpenditureController.cs
@@ -1,17 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using TemplatingRazor.Models
+using TemplatingRazor.Models;
 
 namespace TemplatingRazor.Controllers
 {
     public class ExpenditureController : Controller
     {
-        public IActionResult Index()
+        public IActionResult List()
         {
             List<Expenditure> expenditures = new List<Expenditure>()
             {
-                new Expenditure(1, "Adieu ma retraite", new DateTime(2024, 7, 17), 25000.50)
+                new Expenditure(1, "Adieu ma retraite", new DateTime(2024, 7, 17), 25000.50),
+                new Expenditure(1, "Je suis à la rue", new DateTime(2021, 4, 3), 12555.00),
+                new Expenditure(1, "Merci les allocs", new DateTime(2020, 3, 24), 3)
             };
-            return View();
+            return View("List", expenditures);
         }
     }
 }
